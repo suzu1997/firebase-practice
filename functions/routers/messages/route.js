d40dd9e914d60af894447546ba4c30b5e86ec1c2
Suzuhara Chie <mignon.ange.3@gmail.com>
2021-06-30 23:34:57 +0900

    ⚡️ CRUD処理を行うcloud functionsを実装

--- functions/routers/messages/route.js
+++ functions/routers/messages/route.js
@@ -1,25 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 const endPoint = '/messages';
+
+const db = admin.firestore();
 
 // /messages
 router
   .route(endPoint)
-  .get((req, res) => {
-    res.json({ message: 'Called by the GET method' });
+  .get(async (req, res) => {
+    const messages = [];
+    try {
+      const querySnapshot = await db.collection('messages').get();
+      querySnapshot.forEach((doc) => {
+        messages.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+      res.json({ message: 'Called by the GET method', messages });
+    } catch (error) {
+      console.error(error, 'エラーです！！！！！');
+    }
   })
-  .post((req, res) => {
-    res.json({ message: 'Called by the POST method' });
+  .post(async (req, res) => {
+    const { name, body } = req.body;
+    const createdAt = new Date().toISOString();
+    try {
+      const docRef = await db.collection('messages').add({
+        name,
+        body,
+        createdAt,
+      });
+      const docSnapshot = await docRef.get();
+      const createdMessage = {
+        id: docSnapshot.id,
+        ...docSnapshot.data(),
+      };
+      res.json({
+        message: 'Called by the POST method',
+        data: createdMessage,
+      });
+    } catch (error) {
+      console.error(error, 'エラーです！！！！！');
+    }
   });
 // /messages/1
 router
   .route(`${endPoint}/:id`)
-  .put((req, res) => {
-    res.json({ message: `Called by the PUT method. ID: ${req.params.id}` });
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { name, body } = req.body;
+    const newData = {
+      name,
+      body,
+    };
+    try {
+      await db.collection('messages').doc(id).update(newData);
+      res.json({ message: `Updated!! ID: ${id}` });
+    } catch (error) {
+      res.status(500).res.json({ message: '何かエラーが起きたよ' });
+    }
   })
-  .delete((req, res) => {
-    res.json({ message: `Called by the DELETE method. ID: ${req.params.id}` });
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    try {
+      await db.collection('messages').doc(id).delete();
+      res.json({ message: `Delete data;; ID: ${id}` });
+    } catch (error) {
+      res.status(500).res.json({ message: '何かエラーが起きたよ' });
+    }
   });
 
 module.exports = router;
